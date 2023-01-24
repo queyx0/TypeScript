e058f7d4b72204abd57cce04685bfa47b012ce13
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2023-01-24 12:16:22 -0300

    strictNullChecks

--- Typescript-parte2/app/views/negociacoes-view.ts
+++ Typescript-parte2/app/views/negociacoes-view.ts
@@ -1,11 +1,9 @@
-import { Negociacao } from '../models/negociacao.js';
-import { Negociacoes } from '../models/negociacoes.js';
-import { View } from './view.js';
+import { Negociacoes } from "../models/negociacoes.js";
+import { View } from "./view.js";
 
 export class NegociacoesView extends View<Negociacoes> {
-
-    protected template(model: Negociacoes): string {
-        return `
+  protected template(model: Negociacoes): string {
+    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
@@ -15,7 +13,9 @@ export class NegociacoesView extends View<Negociacoes> {
                 </tr>
             </thead>
             <tbody>
-                ${model.lista().map(negociacao => {
+                ${model
+                  .lista()
+                  .map((negociacao) => {
                     return `
                         <tr>
                             <td>${this.formatar(negociacao.data)}
@@ -28,14 +28,14 @@ export class NegociacoesView extends View<Negociacoes> {
                             </td>
                         </tr>
                     `;
-                }).join('')}
+                  })
+                  .join("")}
             </tbody>
         </table>
         `;
-    }
+  }
 
-    private formatar(data: Date): string {
-        return new Intl.DateTimeFormat()
-            .format(data);
-    }
+  private formatar(data: Date): string {
+    return new Intl.DateTimeFormat().format(data);
+  }
 }
