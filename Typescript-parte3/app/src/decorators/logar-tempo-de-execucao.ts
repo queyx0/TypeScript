6d0491ac4864d03aa88ee4d218690134cd8b7abf
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2023-01-25 09:27:20 -0300

    decorator com parâmetro

--- Typescript-parte3/app/src/decorators/logar-tempo-de-execucao.ts
+++ Typescript-parte3/app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,4 +1,4 @@
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
   return function (
     target: any,
     propertyKey: string,
@@ -6,13 +6,19 @@ export function logarTempoDeExecucao() {
   ) {
     const medotoOriginal = descriptor.value;
     descriptor.value = function (...args: Array<any>) {
+      let divisor = 1;
+      let unidade = "milisegundos";
+      if (emSegundos) {
+        divisor = 1000;
+        unidade = "segundos";
+      }
       const t1 = performance.now();
       const retorno = medotoOriginal.apply(this, args);
       const t2 = performance.now();
       console.log(
-        `${propertyKey}, tempo de execução: ${(t2 - t1) / 1000} segundos`
+        `${propertyKey}, tempo de execução: ${(t2 - t1) / divisor} ${unidade}`
       );
-      retorno;
+      return retorno;
     };
 
     return descriptor;
