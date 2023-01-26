87627bf8e2aa1023c96811e1d2bc0c24c8ec11d7
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2023-01-26 09:56:21 -0300

    finalização da formação com polimorfismo, estendendo interfaces, sourcemaps e debug

--- Typescript-parte3/app/src/controllers/negociacao-controller.ts
+++ Typescript-parte3/app/src/controllers/negociacao-controller.ts
@@ -5,6 +5,7 @@ import { DiasDaSemana } from "../enums/dias-da-semana.js";
 import { Negociacao } from "../models/negociacao.js";
 import { Negociacoes } from "../models/negociacoes.js";
 import { NegociacoesService } from "../services/negociacoes-service.js";
+import { imprimir } from "../utils/imprimir.js";
 import { MensagemView } from "../views/mensagem-view.js";
 import { NegociacoesView } from "../views/negociacoes-view.js";
 
@@ -42,17 +43,27 @@ export class NegociacaoController {
     }
 
     this.negociacoes.adiciona(negociacao);
+    imprimir(negociacao, this.negociacoes);
     this.limparFormulario();
     this.atualizaView();
   }
 
   importaDados(): void {
-    this.negociacaoService.obterNegociacoesDoDia().then((negociacoesDeHoje) => {
-      for (let negociacao of negociacoesDeHoje) {
-        this.negociacoes.adiciona(negociacao);
-      }
-      this.negociacoesView.update(this.negociacoes);
-    });
+    this.negociacaoService
+      .obterNegociacoesDoDia()
+      .then((negociacoesDeHoje) => {
+        return negociacoesDeHoje.filter((negociacoesDeHoje) => {
+          return !this.negociacoes
+            .lista()
+            .some((negociacao) => negociacao.ehIgual(negociacoesDeHoje));
+        });
+      })
+      .then((negociacoesDeHoje) => {
+        for (let negociacao of negociacoesDeHoje) {
+          this.negociacoes.adiciona(negociacao);
+        }
+        this.negociacoesView.update(this.negociacoes);
+      });
   }
 
   private ehDiaUtil(data: Date) {
